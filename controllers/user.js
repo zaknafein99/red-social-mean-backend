298c76b3cd5536a5184b2d4054c3b03fddd6dc53
Ismael <imarquez99@gmail.com>
2018-02-05 23:26:33 -0300

    Metodo de login de usuario

--- controllers/user.js
+++ controllers/user.js
@@ -53,9 +53,6 @@ function saveUser(req, res){
                                 });
                             }
                         });
-
-        
-
     }else{
         res.status(200)({
             message: "Envía todos los campos necesarios"
@@ -63,8 +60,32 @@ function saveUser(req, res){
     }
 }
 
+function loginUser(req, res){
+    var params = req.body;
+
+    var email = params.email;
+    var password = params.password;
+
+    User.findOne({email: email}, (err, user) => {
+        if(err) return res.status(500).send({message: 'Error en la petición'});
+        if(user){
+            bcrypt.compare(password, user.password, (err, check) => {
+                if(check){
+                    // Devolver datos de usuario
+                    return res.status(200).send({user});
+                }else{
+                    return res.status(404).send({message: 'El usuario no se ha podido identificar'});
+                }
+            });
+        }else{
+            return res.status(404).send({message: 'El usuario no se ha podido identificar!'});
+        }
+    });
+}
+
 module.exports= {
     home,
     pruebas,
     saveUser,
+    loginUser,
 }
