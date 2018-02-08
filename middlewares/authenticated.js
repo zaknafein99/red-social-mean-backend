e8d510bd967d3e09f9572653ef7be5b8c538b345
Ismael <imarquez99@gmail.com>
2018-02-07 21:01:35 -0300

    Creado middleware de autenticación

---
+++ middlewares/authenticated.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var secret = 'clave_secreta_red_social_mean';
+
+exports.ensureAuth = function(req, res, next){
+    if(!req.headers.authorization){
+        return res.status(403).send({message: 'La petición no tiene la cabecera de autenticación'})
+    }
+
+    var token = req.headers.authorization.replace(/['"]+/g, '');
+
+    try{
+        var payload = jwt.decode(token, secret);
+
+        if(payload.ex <= moment.unix()){
+            return res.status(401).send({ message: 'El token ha expirado' });
+        }
+    }catch(ex){
+        return res.status(404).send({ message: 'El token no es válido' });
+    }
+    
+    req.user = payload;
+    next();
+}
--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,12 @@
 
 var express = require('express');
 var UserController = require('../controllers/user');
+var md_auth = require('../middlewares/authenticated');
 
 var api = express.Router();
 
 api.get('/home', UserController.home);
-api.get('/pruebas', UserController.pruebas);
+api.get('/pruebas', md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
 
