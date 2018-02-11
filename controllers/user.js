50c34dd554cba7641dc89b0baf1f7f4bb1befcc4
Ismael <imarquez99@gmail.com>
2018-02-11 18:50:46 -0300

    Agregada ruta para devolver los datos de un usuario

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,7 @@ var User = require('../models/user');
 var bcrypt = require('bcrypt-nodejs');
 var jwt = require('../services/jwt');
 
+// Metodos de prueba
 function home(req, res) {
     res.status(200).send({
         message: 'Hola mundo desde el servidor de NodeJS'
@@ -16,6 +17,7 @@ function pruebas(req, res) {
     });
 }
 
+// Registro
 function saveUser(req, res){
     var params = req.body;
     var user = new User();
@@ -61,6 +63,7 @@ function saveUser(req, res){
     }
 }
 
+// Login
 function loginUser(req, res){
     var params = req.body;
 
@@ -93,9 +96,24 @@ function loginUser(req, res){
     });
 }
 
+// Conseguir datos de un usuario
+
+function getUser(req, res){
+    var userId = req.params.id;
+
+    User.findById(userId, (err, user) => {
+        if(err) return res.status(500).send({message: "Error en la petición"});
+
+        if(!user) return res.status(404).send({message: "El usuario no existe"});
+
+        return res.status(200).send({user});
+    });
+}
+
 module.exports= {
     home,
     pruebas,
     saveUser,
     loginUser,
+    getUser,
 }
