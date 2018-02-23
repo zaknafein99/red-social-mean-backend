09edda7ac7116009fcb92bc4c5ed812c525f54cc
Ismael <imarquez99@gmail.com>
2018-02-23 19:07:18 -0300

    Listado de usuarios que sigo

--- controllers/follow.js
+++ controllers/follow.js
@@ -17,7 +17,7 @@ function saveFollow(req, res){
     follow.save((err, followStored) => {
         if(err) return res.status(500).send({message: 'Error al guardar el seguimiento'});
 
-        if(!folloStored) return res.status.send({message: 'El seguimiento no se ha guardado'});
+        if(!followStored) return res.status.send({message: 'El seguimiento no se ha guardado'});
 
         return res.status(200).send({follow:followStored});
         });
@@ -36,7 +36,39 @@ function deleteFollow(req, res){
 	})
 }
 
+function getFollowingUsers(req, res){
+	var userId = req.user.sub;
+
+	if(req.params.id && req.params.page){
+		userId = req.params.id;
+	}
+
+	var page = 1;
+	
+	if(req.params.page){
+		page = req.params.page;
+	}else{
+		page = req.params.id;
+	}
+
+	var itemsPerPage = 4;
+
+	Follow.find({user:userId}).populate({path: 'followed'}).paginate(page, itemsPerPage, (err, follows, total) => {
+		if(err) return res.status(500).send({message: 'Error en el servidor'});
+
+		if(!follows) return res.status(404).send({message: 'No estas siguiendo a ningún usuario'});
+
+		return res.status(200).send({
+			total: total,
+			pages: Math.ceil(total/itemsPerPage),
+			follows
+		})
+	});
+
+}
+
 module.exports = {
 	saveFollow,
-	deleteFollow
+	deleteFollow,
+	getFollowingUsers
 }
