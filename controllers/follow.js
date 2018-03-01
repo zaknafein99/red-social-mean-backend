6c3113d21410485d0d10385976eb5e086634ec53
Ismael <imarquez99@gmail.com>
2018-02-28 21:01:52 -0300

    Listado de seguidores

--- controllers/follow.js
+++ controllers/follow.js
@@ -33,7 +33,7 @@ function deleteFollow(req, res){
 		if(err) return res.status(500).send({message: 'Error al dejar de seguir'});
 
 		return res.status(200).send({message: 'El follow se ha eliminado'});
-	})
+	});
 }
 
 function getFollowingUsers(req, res){
@@ -62,13 +62,46 @@ function getFollowingUsers(req, res){
 			total: total,
 			pages: Math.ceil(total/itemsPerPage),
 			follows
-		})
+		});
 	});
+
+}
+
+function getFollowedUsers(req, res){
+    
+    var userId = req.user.sub;
+    
+    if(req.params.id && req.params.page){
+	    userId = req.params.id;
+    }
+
+    var page = 1;
+
+    if(req.params.page){
+	    page = req.params.page;
+    }else{
+	    page = req.params.id;
+    }
+
+    var itemsPerPage = 4;
+
+    Follow.find({followed:userId}).populate('user').paginate(page, itemsPerPage, (err, follows, total) => {
+	    if(err) return res.status(500).send({message: 'Error en el servidor'});
+
+	    if(!follows) return res.status(404).send({message: 'No te sigue ningún usuario'});
+
+	    return res.status(200).send({
+		    total: total,
+		    pages: Math.ceil(total/itemsPerPage),
+		    follows
+	    });
+    });
 
 }
 
 module.exports = {
 	saveFollow,
 	deleteFollow,
-	getFollowingUsers
+	getFollowingUsers,
+	getFollowedUsers
 }
