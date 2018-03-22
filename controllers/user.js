3517c22df1675b7f9494ff3a536bc72f98a40f70
Ismael <imarquez99@gmail.com>
2018-03-22 17:37:58 -0300

    Verificar si sigo a un usuario

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,14 @@
 /*jslint node: true */
 'use strict';
 
-var User = require('../models/user');
 var bcrypt = require('bcrypt-nodejs');
-var jwt = require('../services/jwt');
 var mongoosePaginate = require('mongoose-pagination');
 var fs = require ('fs');
 var path = require ('path');
+
+var User = require('../models/user');
+var Follow = require('../models/follow');
+var jwt = require('../services/jwt');
 
 // Metodos de prueba
 function home(req, res) {
@@ -109,7 +111,11 @@ function getUser(req, res){
 
         if(!user) return res.status(404).send({message: "El usuario no existe"});
 
-        return res.status(200).send({user});
+        Follow.findOne({"user":req.user.sub, "followed":userId}).exec((err, follow) => {
+            if(err) return res.status(500).send({message: "Error al comprobar el seguimiento"});
+            return res.status(200).send({user, follow});
+        });
+
     });
 }
 
